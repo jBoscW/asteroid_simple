8f1dd29901471b560f22bf60ad6bc845f707a162
jBoscW <jbd393@nyu.edu>
2025-07-31 16:17:23 -0300

    Add less refresh game loop

--- main.py
+++ main.py
@@ -5,9 +5,18 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True: 
-        pygame.Surface.fill(screen, (230,86,192))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
+        pygame.Surface.fill(screen, 'white')
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
         
     
     print("Starting Asteroids!")
